6a2130851d95c0408a636c84a45dd62cb0c2c36b
FabianoEtec <nbscbs@gmail.com>
2021-03-13 13:22:46 -0300

    Finalizado programa que grita

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Beep();
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("VAMOS BRINCAR EM DEIXAR TUDO MAISCULO, AQUILO QUE O AMIGUINHO DIGITAR");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+;           Console.WriteLine("Olá prazer me chamo Fabiano e você, como se chama? ");
+            string nome = Console.ReadLine();
+            Console.WriteLine ($"Seja bem vindo, {nome.ToUpper()}, como foi o seu dia? ");
+            string textoDigitado = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"{textoDigitado.ToUpper()}, Ual!!! {nome.ToUpper()}, o que houve? no proximo encontro quero saber, Tchau.");
+            Console.ResetColor();
+        }
+    }
+}
