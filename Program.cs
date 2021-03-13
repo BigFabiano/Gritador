b24f44837d6603293449f94cac5662eb3aa546c0
FabianoEtec <nbscbs@gmail.com>
2021-03-13 13:54:00 -0300

    Aplicando condição IF e else

--- Program.cs
+++ Program.cs
@@ -12,14 +12,25 @@ namespace Gritador
             Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("VAMOS BRINCAR EM DEIXAR TUDO MAISCULO, AQUILO QUE O AMIGUINHO DIGITAR");
             Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
 ;           Console.WriteLine("Olá prazer me chamo Fabiano e você, como se chama? ");
             string nome = Console.ReadLine();
+
+            if (nome == nome.ToUpper())
+        {
+            Console.WriteLine("Desligue o CAPS LOK");
+     }
+           else
+       {
+    
+
             Console.WriteLine ($"Seja bem vindo, {nome.ToUpper()}, como foi o seu dia? ");
             string textoDigitado = Console.ReadLine();
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine($"{textoDigitado.ToUpper()}, Ual!!! {nome.ToUpper()}, o que houve? no proximo encontro quero saber, Tchau.");
             Console.ResetColor();
+          }
         }
     }
 }
+
